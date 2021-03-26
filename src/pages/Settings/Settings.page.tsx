8e700f22caddc9db90bc10a3c882c77fe186babb
Tristan Matthias <hello@tristanmatthias.com>
2021-03-26 14:37:55 +1100

    fix: settings page loading

--- src/pages/Settings/Settings.page.tsx
+++ src/pages/Settings/Settings.page.tsx
@@ -33,33 +33,28 @@ export default function Settings() {
     setIsLoading(true);
 
     try {
-      await billUser({
-        storage,
-        source: token.id
-      });
-
+      await billUser({ storage, source: token.id });
       // alert('Your card has been charged successfully!');
       history.push('/');
+
     } catch (e) {
       onError(e);
       setIsLoading(false);
     }
   }
 
-  return (
-    <div className="Settings">
-      <StripeProvider stripe={stripe!}>
-        <Elements
-          fonts={[
-            {
-              cssSrc:
-                'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800'
-            }
-          ]}
-        >
-          <BillingForm isLoading={isLoading} onSubmit={handleFormSubmit} />
-        </Elements>
-      </StripeProvider>
-    </div>
-  );
+  if (!stripe) return null;
+
+
+  return <div className="Settings">
+    <StripeProvider stripe={stripe!}>
+      <Elements
+        fonts={[{
+          cssSrc: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800'
+        }]}
+      >
+        <BillingForm isLoading={isLoading} onSubmit={handleFormSubmit} />
+      </Elements>
+    </StripeProvider>
+  </div>;
 }
